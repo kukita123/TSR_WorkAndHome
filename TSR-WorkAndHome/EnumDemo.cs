3eeb97cb6dc8e6d7671885ee02dc7eadc2c18cb1
Mariana Hristozkova <kukita@abv.bg>
2020-09-19 10:37:17 +0300

    Enums demo from home

---
+++ TSR-WorkAndHome/EnumDemo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSR_WorkAndHome
+{
+    // Деклариране на изброяване:
+    enum Animals
+    {
+        Cat,
+        Dog,
+        Fox,
+        Wolf,
+        Bear
+    };
+
+    // Деклариране на изброяване с инициализация на константи:
+    enum Coins { One = 1, Two, Five = 5, Ten = 10, Fifty = 50 };
+
+    class EnumDemo
+    {
+       public static void EnumsTest()
+        {
+            Console.WriteLine("В света на животните");
+            // Променлива от тип изброяване:
+            Animals animal = Animals.Cat;
+            Console.WriteLine("animal: {0,-5} или {1}", animal, (int)animal);
+
+            // Нова стойност на променливата:
+            animal = Animals.Dog;
+            Console.WriteLine("animal: {0,-5} или {1}", animal, (int)animal);
+
+            // Преобразуване на цяло число в стойност от тип
+            // изброяване:
+            animal = (Animals)2;
+            Console.WriteLine("animal: {0,-5} или {1}", animal, (int)animal);
+
+            // Сума на променлива и цяло число:
+            animal = animal + 1;
+            Console.WriteLine("animal: {0,-5} или {1}", animal, (int)animal);
+
+            // Инкрементиране:
+            animal++;
+            Console.WriteLine("animal: {0,-5} или {1}", animal, (int)animal);
+
+
+            Console.WriteLine("В света на финансите");
+            // Променлива от тип на изброяване:
+            Coins coin;
+
+            // Обект с константи от изброяване:
+            // !!! Обърнете внимание на начина на създаване на масива names - с използване на метод GetValues и typeof
+            Array names = Enum.GetValues(typeof(Coins));
+
+            // Обхождане на константите:
+            for (int i = 0; i < names.Length; i++)
+            {
+                // Стойност на променливата:
+                coin = (Coins)names.GetValue(i);
+                Console.WriteLine("coin: {0,-5} или {1}", coin, (int)coin);
+            }
+        }
+    }
+}
--- TSR-WorkAndHome/Program.cs
+++ TSR-WorkAndHome/Program.cs
@@ -8,8 +8,56 @@ namespace TSR_WorkAndHome
 {
     class Program
     {
+        enum Operation
+        {
+            Add = 1,
+            Subtract,
+            Multiply,
+            Divide
+        }
+
+        static void MathOp(double x, double y, Operation op)
+        {
+            double result = 0.0;
+
+            switch (op)
+            {
+                case Operation.Add:
+                    result = x + y;
+                    break;
+
+                case Operation.Subtract:
+                    result = x - y;
+                    break;
+
+                case Operation.Multiply:
+                    result = x * y;
+                    break;
+
+                case Operation.Divide:
+                    result = x / y;
+                    break;
+            }
+
+            Console.WriteLine("Резултат от операция {0} на {1} и {2} равен на {3}",op, x, y, result);
+        }
+
         static void Main(string[] args)
         {
+            Operation op;
+            op = Operation.Add;
+            Console.WriteLine(op); // Add
+
+
+            // Тип на операцията задаваме с помощта на константа Operation.Add, равна 1:
+            MathOp(10, 5, Operation.Add);
+
+            // Типът на операцията задаваме с помощта на константа Operation.Multiply, равна 3:
+            MathOp(11, 5, Operation.Multiply);
+
+            EnumDemo.EnumsTest();
+
+            Console.ReadKey();
         }
     }
 }
