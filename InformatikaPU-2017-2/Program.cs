89c6a8e61bf7326dab48723bf3872a6a4a80c685
Mariana Hristozkova <kukita@abv.bg>
2020-10-03 15:36:40 +0300

    Finishing PU-2017-2

--- InformatikaPU-2017-2/Program.cs
+++ InformatikaPU-2017-2/Program.cs
@@ -15,6 +15,7 @@ namespace InformatikaPU_2017_2
         public string pCode;
         public string city;
 
+        //custom constructor - using string limitation of the parameters
         public Employe(string ime, string EGN, string name, string country, string pCode, string city)
         {
             this.ime = StringLimit(ime, 50);
@@ -24,6 +25,7 @@ namespace InformatikaPU_2017_2
             this.pCode = StringLimit(pCode, 30);       
             this.city = StringLimit(city, 30);
         }
+
         private static string StringLimit(string input, int maxLength)
             {
                 if (input.Length <= 50)
@@ -31,13 +33,17 @@ namespace InformatikaPU_2017_2
 
                 return input.Substring(0, maxLength);
             }
+
         public string DisplayEmploye()
         {
             return ime + ", " + EGN + ", " + name + ", " + country + ", " + city;
         }
     }   
+
+    
     class Program
     {
+        //control the number of new objects
         public static int EmployesNumber()
         {
             Console.Write("Enter number of employes - integer between 1 and 50:");
@@ -53,12 +59,59 @@ namespace InformatikaPU_2017_2
 
             return n;
         }
+
+        //using LINQ to sort the list
+        public static List<Employe> SortByTwoFields(List<Employe> employe)
+        {
+            var sorted = employe.OrderBy(x => x.country).ThenBy(x => x.ime).ToList();
+            return sorted;
+
+        }
+
+        public static List<Employe> SortByEGN(List<Employe> employe)
+        {
+            Employe temp;
+            for (int i = 0; i < employe.Count-1; i++)
+            {
+                for (int j = 0; j < employe.Count; j++)
+                {
+                    if (String.Compare(employe[i].EGN, employe[j].EGN) > 0)
+                    {
+                        temp = employe[i];
+                        employe[i] = employe[j];
+                        employe[j] = temp;
+                    }
+                }
+            }
+            return employe;
+        }
+
+        public static void GenerateEmail(List<Employe> employe)
+        {
+            foreach (var item in employe)
+            {
+                string[]names = item.name.Split(' ');
+                string employeEmail;
+                if (names.Length==3)
+                    employeEmail = names[2] + "_" + names[0] + "_" + names[1].Substring(0, 1) + "@nncomputers.com";
+                else if (names.Length==2)
+                    employeEmail= names[1] + "_" + names[0] + "@nncomputers.com";
+                else
+                    employeEmail = names[0] + "@nncomputers.com";
+
+                Console.WriteLine(item.ime+", email: " + employeEmail);
+            }
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Part 1:");
+
             int n = EmployesNumber();
 
             List<Employe> NewEmployes = new List<Employe>();
 
+            //creating an object using the custom constructor and then adding it to the list:
             for (int i = 0; i < n; i++)
             {
                 Console.Write("Enter ime:");
@@ -78,11 +131,28 @@ namespace InformatikaPU_2017_2
 
                 NewEmployes.Add(employe);
             }
+                        
+            NewEmployes = SortByTwoFields(NewEmployes);
 
+            Console.WriteLine();
+            Console.WriteLine("Part 2:");
             foreach (var item in NewEmployes)
             {
                 Console.WriteLine(item.DisplayEmploye()); 
             }
+            
+            Console.WriteLine();
+            Console.WriteLine("Part 3:");
+            NewEmployes = SortByEGN(NewEmployes);
+            foreach (var item in NewEmployes)
+            {
+                if(item.ime.Length == 0 || item.EGN.Length==0|| item.name.Length==0|| item.country.Length == 0)
+                    Console.WriteLine(item.DisplayEmploye());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Part 4:");
+            GenerateEmail(NewEmployes);
 
             Console.ReadKey();
         }
